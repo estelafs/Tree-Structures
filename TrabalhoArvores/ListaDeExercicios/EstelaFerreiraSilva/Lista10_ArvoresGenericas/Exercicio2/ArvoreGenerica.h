8f7f1d6b1d64eaec0f7e465f265867f11aa18fe1
Estela Ferreira Silva <estelafersil@gmail.com>
2018-06-29 10:36:20 -0300

    Adicionei a lista 10

---
+++ TrabalhoArvores/ListaDeExercicios/EstelaFerreiraSilva/Lista10_ArvoresGenericas/Exercicio2/ArvoreGenerica.h
@@ -0,0 +1,28 @@
+#ifndef ARVOREGENERICA_H_INCLUDED
+#define ARVOREGENERICA_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct no{
+	int info;
+	struct no *esq; //a esquerda são filhos
+	struct no *dir; //a direita são irmãos
+};
+
+typedef struct no Arv;
+
+Arv * cria_arvore(int elem);
+int arvore_vazia(Arv * a);
+
+int insere_como_filha(Arv * mae, Arv * filha);
+
+void exibe_arvore(Arv * a);
+
+int busca_elem(Arv * a, int elem);
+
+void libera_arvore(Arv * a);
+
+int altura(Arv * a, int cont);
+
+#endif
